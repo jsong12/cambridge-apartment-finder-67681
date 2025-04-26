4c1e70a8cf34d31ee992f628424ea25b5030214d
jsong12 <41485903+jsong12@users.noreply.github.com>
2025-04-25 20:41:19 -0400

    Update fetchListings.js

--- netlify/functions/fetchListings.js
+++ netlify/functions/fetchListings.js
@@ -1,10 +1,14 @@
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-const cheerio = require('cheerio'); // We need this to scrape HTML
+const cheerio = require('cheerio');
 
 exports.handler = async function () {
   try {
     const url = 'https://boston.craigslist.org/search/apa?query=Cambridge&bedrooms=1&availabilityMode=0&max_price=2800';
-    const res = await fetch(url);
+    const res = await fetch(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    });
     const html = await res.text();
     const $ = cheerio.load(html);
 
